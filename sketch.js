e9e37b36617e9727d92373872c0dfbc62365d90e
jsphkm <jsphkm12@gmail.com>
2019-03-03 07:57:53 -0800

    Create snake instance  - Use a constructor function

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+let s;
+
 function setup() {
   createCanvas(600, 600);
+  s = new Snake();
 }
 
 function draw() {
+ background(51);
+ s.update();
+ s.show();
+}
 
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+
+  this.update = () => {
+    this.x = this.x + this.xspeed;
+    this.y = this.y + this.yspeed;
+  };
+
+  this.show = () => {
+    fill(255);
+    rect(this.x, this.y, 10, 10);
+  }
 }
