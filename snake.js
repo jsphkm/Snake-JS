4a9d8b84111a8382ab6cf5b046d0c0f2d141a423
jsphkm <jsphkm12@gmail.com>
2019-03-03 11:35:24 -0800

    End game when snake is outside of the canvas

--- snake.js
+++ snake.js
@@ -13,18 +13,41 @@ class Snake {
   }
 
   update() {
-    this.body[0].x += this.xdir;
-    this.body[0].y += this.ydir;
+    let head = this.body[this.body.length - 1].copy();
+    this.body.shift();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.push(head);
+
+    // this.body[0].x += this.xdir;
+    // this.body[0].y += this.ydir;
   }
 
   grow() {
+    let head = this.body[this.body.length - 1].copy();
     this.len += 1;
-    this.body.push(createVector(0, 0));
+    this.body.push(head);
+  }
+
+  endGame() {
+    let x = this.body[this.body.length - 1].x;
+    let y = this.body[this.body.length - 1].y;
+    if (x > w - 1 || x < 0 || y > h - 1 || y < 0) {
+      return true;
+    }
+
+    for (let i = 0; i < this.body.length - 1; i += 1) {
+      const part = this.body[i];
+      if (part.x === x && part.y === y) {
+        return true;
+      }
+    };
+    return false;
   }
 
   eat(pos) {
-    let x = this.body[0].x;
-    let y = this.body[0].y;
+    let x = this.body[this.body.length - 1].x;
+    let y = this.body[this.body.length - 1].y;
     if (x === pos.x && y === pos.y) {
       this.grow();
       return true;
