9def4d496db2ee766299fb1f3708c485cd63eb50
Joseph Kim <28643797+jsphkm@users.noreply.github.com>
2026-08-05 05:01:12 -0400

    Add viewport meta so mobile web scales to the device

--- rn/src/app/+html.tsx
+++ rn/src/app/+html.tsx
@@ -1,5 +1,6 @@
 import { ScrollViewStyleReset } from "expo-router/html";
 import { palette } from "../theme";
+
 const css = `
     html, body, #root { height: 100%; }
     body { margin: 0; background: ${palette.light.page}; }
@@ -9,13 +10,19 @@ const css = `
 `;
 
 export default function Root({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-            <head>
-                <ScrollViewStyleReset />
-                <style dangerouslySetInnerHTML={{ __html: css }} />
-            </head>
-            <body>{children}</body>
-        </html>
-    );
-};
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+        />
+        <ScrollViewStyleReset />
+        <style dangerouslySetInnerHTML={{ __html: css }} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
